a25f56a51e90f3e1b2789e2b146d63587737ff8e
Kerim YASAR <kerim.yasar@viacesi.fr>
2020-06-23 11:43:47 +0200

    LoginForm OK

---
+++ ClientApplication/Models/FileManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientApplication.Models
+{
+    public class FileManager
+    {
+        string fileName;
+
+        int fileLength;
+
+        string path;
+
+        string fileContent;
+
+        public void addFiles()
+        {
+        }
+    }
+}
---
+++ ClientApplication/Models/TokenManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientApplication.Models
+{
+    public class TokenManager
+    {
+        string tokenApp;
+
+        string tokenUser;
+
+        public void generateTokenApp()
+        {
+
+        }
+        public void generateTokenUser()
+        {
+
+        }
+    }
+}
