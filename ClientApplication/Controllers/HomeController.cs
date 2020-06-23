9d2c1645815107cd5224dcd16036282515a3be51
sambao407 <sambao407@gmail.com>
2020-06-23 14:23:13 +0200

    Add SQL Query

--- ClientApplication/Controllers/HomeController.cs
+++ ClientApplication/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +10,9 @@ namespace ClientApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private String username;
+        private String password;
+        private String tokenapp;
         public ActionResult Index()
         {
             return View();
@@ -20,5 +25,13 @@ namespace ClientApplication.Controllers
 
             return View();
         } */
+
+        public void logButton(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=GEARLESS-JOE;Initial Catalog=ClientDatabase;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter("Select FirstName,UserPassword, TokenApp From UserList where Firstname ='" + username + "' and UserPassword ='" + password + "' and TokenApp='"+ tokenapp +"'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+        }
     }
 }
