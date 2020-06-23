8e8977ed159e5551b17b8500c0c2c4cabdf47d05
Kerim YASAR <kerim.yasar@viacesi.fr>
2020-06-23 16:34:15 +0200

    Test

---
+++ ClientApplication/Controllers/AuthentificationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientApplication.Controllers
+{
+    public class AuthentificationController : Controller
+    {
+        public ActionResult AuthentificationPage()
+        {
+            return View();
+        }
+    }
+}
