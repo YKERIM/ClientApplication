532d8c6f52516ff0a317afc41a20d4cfa42ef7f8
Kerim YASAR <kerim.yasar@viacesi.fr>
2020-06-23 09:46:56 +0200

    Page de base créée

--- ClientApplication/Controllers/HomeController.cs
+++ ClientApplication/Controllers/HomeController.cs
@@ -13,25 +13,12 @@ namespace ClientApplication.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult LoginPage()
+   /*     public ActionResult LoginPage()
         {
             ViewBag.Message = "Veuillez vous connectez";
 
             return View();
-        }
+        } */
     }
 }
