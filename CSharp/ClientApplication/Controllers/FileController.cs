29f19e18b7d82f7a6ea6ea73b091696e3dc5767b
sambao407 <sambao407@gmail.com>
2020-06-25 15:54:07 +0200

    Upload Files code

---
+++ CSharp/ClientApplication/Controllers/FileController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientApplication.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public ActionResult SaveFiles()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveFiles(HttpPostedFileBase UploadedFile)
+        {
+            if (UploadedFile.ContentLength > 0)
+            {
+                string EncryptFileName = Path.GetFileName(UploadedFile.FileName);
+                string FolderPath = Path.Combine(Server.MapPath("~/UploadedFiles"), EncryptFileName);
+
+                UploadedFile.SaveAs(FolderPath);
+            }
+
+            ViewBag.Message = "Vos fichiers ont été insérés avec succès";
+
+            return View();
+        }
+    }
+}
