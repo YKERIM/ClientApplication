a4a0963cf2aeb16f7fd0ef1d0db7c0c8b59d8146
Kerim YASAR <kerim.yasar@viacesi.fr>
2020-06-25 12:34:06 +0200

    Connexion OK, Authentfication OK
    Erreur d'instanciation présente

--- CSharp/ClientApplication/Controllers/HomeController.cs
+++ CSharp/ClientApplication/Controllers/HomeController.cs
@@ -6,11 +6,13 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebGrease.Css.Ast.Selectors;
 
 namespace ClientApplication.Controllers
 {
     public class HomeController : Controller
     {
+        public List<User> user_saved = new List<User>();
         public SqlConnection con = new SqlConnection(@"Data Source=JIREN-SAMA;Initial Catalog=ClientApplication;Integrated Security=True");
 
         public ActionResult Index()
@@ -24,23 +26,50 @@ namespace ClientApplication.Controllers
         }
 
         [HttpPost]
-        public ContentResult Index(User user)
+        public ActionResult Index(User user)
         {
-
             SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From UserList where Firstname ='" + user.userName + "' and UserPassword ='" + user.password + "'", con);
             DataTable dt = new DataTable();
             sda.Fill(dt);
             if (dt.Rows[0][0].ToString() == "1")
             {
-                Response.Redirect("Home/Authentification");
+                user_saved.Add(user);
+                Response.Redirect("~/Home/Authentification");
             }
             else
             {
-                ViewBag.Message("Invalid username or password");
+                ViewBag.Message = user;
             }
 
-            return Content(user.userName + " " + user.password);
+            return View();
         }
 
+        [HttpPost]
+        public ActionResult Authentification(User user)
+        {
+            foreach (var current_user in user_saved)
+            {
+                if (current_user.tokenApp == user.tokenApp)
+                {
+                    user.userName = current_user.userName;
+                    user.password = current_user.password;
+                }
+            }
+
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From UserList where Firstname ='" + user.userName + "' and UserPassword ='" + user.password + "' and TokenApp = '" + user.tokenApp + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                Response.Redirect("~/Home/DecryptagePage");
+            }
+            else
+            {
+                ViewBag.Message = user;
+            }
+
+            return View();
+        }
     }
 }
